1caf5b6455bd2b86065a39822974fcdc2794fc86
Pavel Lepekhin <pavel.lepekhin@sbb.ch>
2022-01-04 00:19:51 +0300

    fix timezone 2

--- src/main/java/com/lyapizz/emmacalendar/service/AgeService.java
+++ src/main/java/com/lyapizz/emmacalendar/service/AgeService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.ZoneId;
 
 @Service
 public class AgeService {
@@ -14,7 +15,7 @@ public class AgeService {
     private static final LocalDate EMMA_BIRTHDAY = LocalDate.of(2021, 10, 25);
 
     public AgeResponse getAge() {
-        LocalDate currentDate = LocalDate.now();
+        LocalDate currentDate = LocalDate.now(ZoneId.of("Europe/Moscow"));
         Period agePeriod = Period.between(EMMA_BIRTHDAY, currentDate);
         return new AgeResponse(periodConverter.convert(agePeriod));
     }
